a4e610021f081c83cc265f839afd6daa569b2bf7
Alice Chen <alicejychen15@gmail.com>
2019-05-17 22:43:30 -0400

    navbar

--- client/components/Navbar/index.js
+++ client/components/Navbar/index.js
@@ -10,9 +10,15 @@ class Navbar extends Component {
     return (
       <div>
         <nav className="">
-          <a className="title" style={{ color:'#747474' }} href="">simonhuang.</a>
+          <img className="logo" src="img/simonLogo.png" />
+          <a className="title" href="">simonhuang.</a>
+          <ul>
+            <li><a href="">equipment</a></li>
+            <li><a href="">about</a></li>
+            <li><a href="">contact</a></li>
+          </ul>
         </nav>
-        <div className="c"></div>
+       
       </div>
     )
   }
